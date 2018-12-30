3f2f7e3bd021e9d8523694bf9666f897bb51ba23
Julie Moon <8juliemoon@gmail.com>
2018-12-29 22:26:43 -0500

    Entire setup success with React & Webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>HI JULIE, i love you SOO MUCH</div>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    contentBase: './public'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react']
+          }
+        }
+      },
+      {
+        test: /\.$css/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ]
+      }
+    ]
+  }
+};
